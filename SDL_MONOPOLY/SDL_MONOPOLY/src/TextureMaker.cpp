979a054d8063d9c1bd6e4134b5901e01eb3548a6
catalin Caprita <cata.caprita9@gmail.com>
2020-04-23 13:05:44 +0300

    updated Folders

---
+++ SDL_MONOPOLY/SDL_MONOPOLY/src/TextureMaker.cpp
@@ -0,0 +1,30 @@
+#include "../headers/TextureMaker.h"
+SDL_Surface* TextureMaker::temp = NULL;
+SDL_Renderer* TextureMaker::link = NULL;
+TextureMaker::TextureMaker(){}
+TextureMaker::~TextureMaker() {
+	SDL_FreeSurface(temp);
+}
+
+void TextureMaker::attach(SDL_Renderer* ren) {
+	link = ren;
+}
+SDL_Texture* TextureMaker::textureFromBMP(const char* path) {
+	temp = SDL_LoadBMP(path);
+	if (link == NULL)
+		std::cout << "LINK IS NULL" << std::endl;
+	if (temp == NULL) {
+		std::cout << "Could not load Image: " << SDL_GetError() << std::endl;
+		return NULL;
+	}
+	return SDL_CreateTextureFromSurface(link,temp);
+}
+SDL_Texture* TextureMaker::getWindowTexture(SDL_Window* win) {
+	temp = SDL_GetWindowSurface(win);
+	if (temp == NULL) {
+		std::cerr << "Could not load from Window: " << SDL_GetError() << std::endl;
+		return NULL;
+	}
+	return SDL_CreateTextureFromSurface(link,temp);
+
+}
