756943c819f12558bf7efdc9a63218f21c3209d3
Alen <47917027+alenmemenduf@users.noreply.github.com>
2020-06-14 20:16:42 +0300

    hotfix

--- SDL_MONOPOLY/SDL_MONOPOLY/src/Game.cpp
+++ SDL_MONOPOLY/SDL_MONOPOLY/src/Game.cpp
@@ -223,7 +223,6 @@ Dice* Game::getDice() {
 								 /*
 								 players[turn]->setRemainingSteps(1);
 								 */
-								 dice->setBlocked(true);
 								 if (!dice->thrownDouble()) {
 									 dice->setBlocked(true);
 									 Game::nrDoublesThrown = 0;
